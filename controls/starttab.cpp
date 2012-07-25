4be6945f058c9e83074d1b34b901062541bc74e9
Vladimir <riuson@gmail.com>
2012-07-25 23:35:44 +0600

    Added copyright to source files

--- controls/starttab.cpp
+++ controls/starttab.cpp
@@ -1,6 +1,6 @@
 /*
  * LCD Image Converter. Converts images and fonts for embedded applciations.
- * Copyright (C) 2010 riuson
+ * Copyright (C) 2012 riuson
  * mailto: riuson@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
