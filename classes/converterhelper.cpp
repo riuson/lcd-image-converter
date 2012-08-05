75ff1e4c977909aa8b60a93852253bfeee377e15
Vladimir <riuson@gmail.com>
2012-08-05 15:51:07 +0600

    Implemented basic algorithms to process image pixels with mask and shift

--- classes/converterhelper.cpp
+++ classes/converterhelper.cpp
@@ -20,6 +20,9 @@
 #include "converterhelper.h"
 //-----------------------------------------------------------------------------
 #include <QStringList>
+#include <QImage>
+#include <QColor>
+#include <QPainter>
 //-----------------------------------------------------------------------------
 void ConverterHelper::packDataPreview(QStringList *list, QStringList &colors, int bits, bool pack, bool alignToHigh)
 {
@@ -57,5 +60,262 @@ void ConverterHelper::packDataPreview(QStringList *list, QStringList &colors, in
     }
     list->append(temp);
     temp.clear();
+}
+//-----------------------------------------------------------------------------
+void ConverterHelper::createMatrixMono(QList<quint32> *matrix, bool pack, MonochromeType type, int edge)
+{
+    if (matrix != NULL)
+    {
+        matrix->clear();
+        quint32 options = 0;
+
+        if (pack)
+            options |= MaskPackData;
+        options |= ConversionTypeMonochrome << 28;
+        options |= type << 24;
+        options |= (edge & 0xff) << 16;
+        options |= Data8 << 12;
+
+        matrix->append(options); // conversion options
+
+        matrix->append(0x00000001); // mask of used bits before packing
+
+        // bits shift
+        {
+            matrix->append(0x00000001);
+
+            matrix->append(0x00000000);
+        }
+    }
+}
+//-----------------------------------------------------------------------------
+void ConverterHelper::createMatrixGrayscale(QList<quint32> *matrix, bool pack, int bits)
+{
+    if (matrix != NULL)
+    {
+        matrix->clear();
+
+        if (bits > 8) bits = 8;
+        if (bits < 1) bits = 1;
+
+        // options
+        {
+            quint32 options = 0;
+
+            if (pack)
+                options |= MaskPackData;
+            options |= ConversionTypeGrayscale << 28;
+            options |= Data16 << 12;
+
+            matrix->append(options); // conversion options
+        }
+
+        // mask of used bits before packing
+        {
+            quint32 mask = 0x000000ff;
+            mask = mask << bits;
+            mask = mask >> 8;
+            mask = mask & 0x000000ff;
+            matrix->append(mask);
+        }
+
+        // bits shift
+        {
+            quint32 mask = 0x0000ff00;
+            mask = mask >> bits;
+            mask = mask & 0x000000ff;
+            matrix->append(mask);
+
+            quint32 shift = 8 - bits;
+            matrix->append(shift);
+        }
+    }
+}
+//-----------------------------------------------------------------------------
+void ConverterHelper::createMatrixColor(QList<quint32> *matrix, bool pack, int redBits, int greenBits, int blueBits)
+{
+    if (matrix != NULL)
+    {
+        matrix->clear();
+
+        if (redBits > 8) redBits = 8;
+        if (redBits < 1) redBits = 1;
+        if (greenBits > 8) greenBits = 8;
+        if (greenBits < 1) greenBits = 1;
+        if (blueBits > 8) blueBits = 8;
+        if (blueBits < 1) blueBits = 1;
+
+        int bits = redBits + greenBits + blueBits;
+
+        // options
+        {
+            quint32 options = 0;
+
+            if (pack)
+                options |= MaskPackData;
+            options |= ConversionTypeColor << 28;
+            options |= Data32 << 12;
+
+            matrix->append(options); // conversion options
+        }
+
+        // mask of used bits before packing
+        {
+            quint64 mask64 = 0x00000000ffffffff;
+            mask64 = mask64 << bits;
+            mask64 = mask64 >> 32;
+            mask64 = mask64 & 0x0000000000ffffff; // 24 bits
+            quint32 mask = (quint32)mask64;
+            matrix->append(mask);
+        }
+
+        // red bits shift
+        {
+            quint32 mask = 0x0000ff00;
+            mask = mask >> redBits;
+            mask = mask & 0x000000ff;
+            mask = mask << 16;
+            matrix->append(mask);
+
+            quint32 shift = 24 - bits;
+            matrix->append(shift);
+        }
+
+        // green bits shift
+        {
+            quint32 mask = 0x0000ff00;
+            mask = mask >> greenBits;
+            mask = mask & 0x000000ff;
+            mask = mask << 8;
+            matrix->append(mask);
+
+            quint32 shift = 16 - greenBits - blueBits;
+            matrix->append(shift);
+        }
+
+        // blue bits shift
+        {
+            quint32 mask = 0x0000ff00;
+            mask = mask >> blueBits;
+            mask = mask & 0x000000ff;
+            matrix->append(mask);
+
+            quint32 shift = 8 - blueBits;
+            matrix->append(shift);
+        }
+    }
+}
+//-----------------------------------------------------------------------------
+void ConverterHelper::pixelsData(QList<quint32> *matrix, QImage *image, QList<quint32> *data, int *width, int *height)
+{
+    if (image != NULL && data != NULL && width != NULL && height != NULL)
+    {
+        data->clear();
+
+        QImage im = *image;
+
+        *width = im.width();
+        *height = im.height();
+
+        // monochrome image needs special preprocessing
+        ConversionType type = (ConversionType)((matrix->at(0) & MaskConversionType) >> 28);
+        if (type == ConversionTypeMonochrome)
+        {
+            MonochromeType monotype = (MonochromeType)((matrix->at(0) & MaskMonochromeType) >> 24);
+            int edge = (matrix->at(0) & MaskEdgeValue) >> 16;
+
+            switch (monotype)
+            {
+            case MonochromeTypeEdge:
+                ConverterHelper::makeMonochrome(im, edge);
+                break;
+            case MonochromeTypeDiffuseDither:
+                im = image->convertToFormat(QImage::Format_Mono, Qt::MonoOnly | Qt::DiffuseDither);
+                break;
+            case MonochromeTypeOrderedDither:
+                im = image->convertToFormat(QImage::Format_Mono, Qt::MonoOnly | Qt::OrderedDither);
+                break;
+            case MonochromeTypeThresholdDither:
+                im = image->convertToFormat(QImage::Format_Mono, Qt::MonoOnly | Qt::ThresholdDither);
+                break;
+            }
+        }
+        else if (type == ConversionTypeGrayscale)
+        {
+            ConverterHelper::makeGrayscale(im);
+        }
+
+        for (int y = 0; y < im.height(); y++)
+        {
+            for (int x = 0; x < im.width(); x++)
+            {
+                // typedef QRgb
+                // An ARGB quadruplet on the format #AARRGGBB, equivalent to an unsigned int.
+                QRgb pixel = im.pixel(x, y);
+                quint32 value = pixel & 0x00ffffff;
+                data->append(value);
+            }
+        }
+    }
+}
+//-----------------------------------------------------------------------------
+void ConverterHelper::processPixels(QList<quint32> *matrix, QList<quint32> *data)
+{
+    if (matrix != NULL && data != NULL)
+    {
+        for (int i = 0; i < data->length(); i++)
+        {
+            quint32 value = data->at(i);
+            quint32 valueNew = 0;
+            for (int j = 2; j < matrix->length(); j += 2)
+            {
+                quint32 mask = matrix->at(i);
+                quint32 shift = matrix->at(i + 1);
+
+                if (shift & 0x80000000)
+                    valueNew |= (value & mask) << (shift & 0x0000001f);
+                else
+                    valueNew |= (value & mask) >> (shift & 0x0000001f);
+            }
+            valueNew &= matrix->at(1);
+            data->replace(i, valueNew);
+        }
+    }
+}
+//-----------------------------------------------------------------------------
+void ConverterHelper::makeMonochrome(QImage &image, int edge)
+{
+    QPainter painter(&image);
+    painter.setRenderHint(QPainter::Antialiasing, false);
+    edge &= 0x000000ff;
+    for (int x = 0; x < image.width(); x++)
+    {
+        for (int y = 0; y < image.height(); y++)
+        {
+            QRgb value = image.pixel(x, y);
+            if (qGray(value) < edge)
+                painter.setPen(QColor(0, 0, 0));
+            else
+                painter.setPen(QColor(255, 255, 255));
+            painter.drawPoint(x, y);
+        }
+    }
+}
+//-----------------------------------------------------------------------------
+void ConverterHelper::makeGrayscale(QImage &image)
+{
+    QPainter painter(&image);
+    painter.setRenderHint(QPainter::Antialiasing, false);
+    for (int x = 0; x < image.width(); x++)
+    {
+        for (int y = 0; y < image.height(); y++)
+        {
+            QRgb value = image.pixel(x, y);
+            int gray = qGray(value);
+            QColor color = QColor(gray ,gray, gray);
+            painter.setPen(color);
+            painter.drawPoint(x, y);
+        }
+    }
 }
 //-----------------------------------------------------------------------------
--- classes/converterhelper.h
+++ classes/converterhelper.h
@@ -20,12 +20,90 @@
 #ifndef CONVERTERHELPER_H
 #define CONVERTERHELPER_H
 //-----------------------------------------------------------------------------
+#include <QtGlobal>
+//-----------------------------------------------------------------------------
 class QStringList;
+class QImage;
+template <class T> class QList;
+//-----------------------------------------------------------------------------
+enum ConversionType
+{
+    ConversionTypeMonochrome = 0,
+    ConversionTypeGrayscale  = 1,
+    ConversionTypeColor      = 2
+};
+//-----------------------------------------------------------------------------
+enum MonochromeType
+{
+    MonochromeTypeEdge = 0,
+    MonochromeTypeDiffuseDither = 1,
+    MonochromeTypeOrderedDither = 2,
+    MonochromeTypeThresholdDither = 3
+};
+//-----------------------------------------------------------------------------
+enum ColorsOrder
+{
+    ColorsOrderRGB = 0,
+    ColorsOrderRBG = 1,
+    ColorsOrderGRB = 2,
+    ColorsOrderGBR = 3,
+    ColorsOrderBRG = 4,
+    ColorsOrderBGR = 5
+};
+//-----------------------------------------------------------------------------
+enum DataBlockSize
+{
+    Data8  = 0,
+    Data16 = 1,
+    Data24 = 2,
+    Data32 = 3
+};
 //-----------------------------------------------------------------------------
 class ConverterHelper
 {
 public:
+
     static void packDataPreview(QStringList *list, QStringList &colors, int bits, bool pack, bool alignToHigh);
+
+    static void createMatrixMono(QList<quint32> *matrix, bool pack = true, MonochromeType type = MonochromeTypeDiffuseDither, int edge = 0x80);
+    static void createMatrixGrayscale(QList<quint32> *matrix, bool pack = true, int bits = 8);
+    static void createMatrixColor(QList<quint32> *matrix, bool pack = true, int redBits = 8, int greenBits = 8, int blueBits = 8);
+
+    // gets RGB array of pixels
+    // also convert images to monochrome or grayscale (r = g = b = qGray(pixel))
+    static void pixelsData(QList<quint32> *matrix, QImage *image, QList<quint32> *data, int *width, int *height);
+    // apply masks and bits reorder
+    static void processPixels(QList<quint32> *matrix, QList<quint32> *data);
+private:
+    static const quint32 MaskPackData       = 0x80000000;
+    static const quint32 MaskByteOrder      = 0x40000000;
+    static const quint32 MaskConversionType = 0x30000000;
+    static const quint32 MaskMonochromeType = 0x0f000000;
+    static const quint32 MaskEdgeValue      = 0x00ff0000;
+    static const quint32 MaskDataBlockSize  = 0x0000f000;
+
+    static void makeMonochrome(QImage &image, int edge);
+    static void makeGrayscale(QImage &image);
 };
+/*
+ *  Matrix structure:
+ *
+ * [0] ConversionOptions:
+ *     31    - pack data
+ *     30    - byte order, 0 = little-endian, 1 = big-endian
+ *     29:28 - conversion type
+ *     27:24 - monochrome type
+ *     23:16 - edge value for MonochromeTypeEdge
+ *     15:12 - data block size
+ *
+ * [1] Mask of used bits before packing
+ *
+ * [2] Mask of data bit
+ * [3] Shift of data bit:
+ *     31    - 0 = to right, 1 = to left
+ *     4:0   - count
+ *
+ * ... Repeat 2 and 3 some times
+ */
 //-----------------------------------------------------------------------------
 #endif // CONVERTERHELPER_H
