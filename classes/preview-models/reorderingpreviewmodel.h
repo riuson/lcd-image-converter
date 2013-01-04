70f7682359d76151f9221d35908bb1a17e94995a
Vladimir <riuson@gmail.com>
2013-01-04 21:25:36 +0600

    Date fixed

--- classes/preview-models/reorderingpreviewmodel.h
+++ classes/preview-models/reorderingpreviewmodel.h
@@ -1,6 +1,6 @@
 /*
  * LCD Image Converter. Converts images and fonts for embedded applications.
- * Copyright (C) 2012 riuson
+ * Copyright (C) 2013 riuson
  * mailto: riuson@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
