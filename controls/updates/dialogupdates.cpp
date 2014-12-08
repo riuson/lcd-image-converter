100ea8a961568518077828699448263b83ba3455
Vladimir <riuson@gmail.com>
2014-12-08 19:38:32 +0500

    Changed uri for updates file

--- controls/updates/dialogupdates.cpp
+++ controls/updates/dialogupdates.cpp
@@ -113,7 +113,7 @@ void DialogUpdates::showUpdates()
     QNetworkAccessManager* mNetworkManager = new QNetworkAccessManager(this);
     this->connect(mNetworkManager, SIGNAL(finished(QNetworkReply*)), SLOT(networkReply(QNetworkReply*)));
 
-    QUrl url("http://riuson.github.io/lcd-image-converter/history/history.xml");
+    QUrl url("http://lcd-image-converter.riuson.com/history/history.xml");
     QNetworkReply* reply = mNetworkManager->get(QNetworkRequest(url));
     Q_UNUSED(reply);
 }
