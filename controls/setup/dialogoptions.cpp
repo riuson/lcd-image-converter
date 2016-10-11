7fb2e36c190c48ad1f1b9ff04c8eff3bdf6210ae
Vladimir <riuson@gmail.com>
2016-10-11 17:02:38 +0500

    Select imported preset

--- controls/setup/dialogoptions.cpp
+++ controls/setup/dialogoptions.cpp
@@ -111,7 +111,7 @@ DialogOptions::~DialogOptions()
     delete this->mPreset;
 }
 //-----------------------------------------------------------------------------
-void DialogOptions::fillPresetsList()
+void DialogOptions::fillPresetsList(const QString &defaultName)
 {
     this->disconnect(this->ui->comboBoxPresets, SIGNAL(currentIndexChanged(int)), this, SLOT(on_comboBoxPresets_currentIndexChanged(int)));
 
@@ -120,13 +120,28 @@ void DialogOptions::fillPresetsList()
     this->ui->comboBoxPresets->clear();
 
     QStringList names = Preset::presetsList();
-
     this->ui->comboBoxPresets->addItems(names);
 
-    if (names.contains(current))
+    bool defaultLoaded = false;
+
+    if (!defaultName.isEmpty())
     {
-        this->ui->comboBoxPresets->setCurrentIndex(names.indexOf(current));
-        this->presetLoad(current);
+        if (names.contains(defaultName))
+        {
+            this->ui->comboBoxPresets->setCurrentIndex(names.indexOf(defaultName));
+            this->presetLoad(defaultName);
+            defaultLoaded = true;
+        }
+    }
+
+    if (!defaultLoaded)
+    {
+        if (names.contains(current))
+        {
+            this->ui->comboBoxPresets->setCurrentIndex(names.indexOf(current));
+            this->presetLoad(current);
+        }
+
     }
 
     this->connect(this->ui->comboBoxPresets, SIGNAL(currentIndexChanged(int)), SLOT(on_comboBoxPresets_currentIndexChanged(int)));
@@ -280,7 +295,7 @@ void DialogOptions::on_pushButtonPresetImport_clicked()
 
         delete importedPreset;
 
-        this->fillPresetsList();
+        this->fillPresetsList(resultPresetName);
     }
 }
 //-----------------------------------------------------------------------------
