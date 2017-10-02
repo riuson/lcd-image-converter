5744610267aa17902e8531d7d1d6326aee2f0ffb
Vladimir <riuson@gmail.com>
2017-10-02 23:47:47 +0500

    Script example updated

--- resources/scan_scripts/pixels_template.js
+++ resources/scan_scripts/pixels_template.js
@@ -1,9 +1,9 @@
 var __internal_points_count = data.count;
 
 for (var __internal_point_index = 0; __internal_point_index < __internal_points_count; __internal_point_index++) {
-    var argb_uint32 = data.pixel(__internal_point_index);
+    var argb = data.pixel(__internal_point_index);
     {
         %1
     }
-    data.setPixel(__internal_point_index, argb_uint32);
+    data.setPixel(__internal_point_index, argb);
 }
